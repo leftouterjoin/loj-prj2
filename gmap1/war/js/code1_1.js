5fc457ae2bbb5310cd3cbcc771b12300a99cbd15
leftouterjoin <leftouterjoin@48dbb0f8-4a9e-11de-b449-6f096a66a0e6>
2010-09-06 01:57:15 +0000

    http://www.ajaxtower.jp/googlemaps/route/index1.html

---
+++ gmap1/war/js/code1_1.js
@@ -0,0 +1,6 @@
+function initialize() {
+  if (GBrowserIsCompatible()) {
+    var map = new GMap2(document.getElementById("map_canvas"));
+    map.setCenter(new GLatLng(41.7724,140.72628), 12);
+  }
+}
